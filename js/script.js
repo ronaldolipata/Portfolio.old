72992582d53aa1e76d5624e536a673f4fd4d7385
Ronaldo Lipata <lipata.ronaldo@gmail.com>
2022-07-05 21:12:34 +0800

    Fix social icons

--- js/script.js
+++ js/script.js
@@ -11,6 +11,12 @@ let body = document.querySelector("body");
 let nav = document.querySelector("nav");
 let logo = document.querySelector(".logo");
 let about = document.querySelector(".about");
+let heroLinkedIn = document.querySelector(".hero-linkedin");
+let heroGitHub = document.querySelector(".hero-github");
+let heroDribble = document.querySelector(".hero-dribbble");
+let connectLinkedIn = document.querySelector(".connect-linkedin");
+let connectGitHub = document.querySelector(".connect-github");
+let connectDribble = document.querySelector(".connect-dribbble");
 let technologies = document.querySelector(".technologies");
 let projects = document.querySelector(".projects");
 let connect = document.querySelector(".connect");
@@ -66,8 +72,16 @@ theme.addEventListener("change", event => {
     body.classList.add("dark");
     nav.classList.add("dark");
     nav.classList.add("add-shadow.dark");
-    logo.style.backgroundImage = "url('../img/logo-dark.svg')";
-    technologies.src ="img/dark-technologies.svg";
+    logo.src = "../img/logo-dark.svg";
+    heroLinkedIn.src = "../img/dark-linkedin.svg";
+    heroGitHub.src = "../img/dark-github.svg";
+    heroDribble.src = "../img/dark-dribbble.svg";
+
+    connectLinkedIn.src = "../img/dark-linkedin.svg";
+    connectGitHub.src = "../img/dark-github.svg";
+    connectDribble.src = "../img/dark-dribbble.svg";
+
+    technologies.src ="../img/dark-technologies.svg";
     projects.style.backgroundImage = "url('../img/code-backgroundv2.svg')";
     footer.classList.add("dark");
 
@@ -79,7 +93,16 @@ theme.addEventListener("change", event => {
     body.classList.remove("dark");
     nav.classList.remove("dark");
     about.classList.remove("dark");
-    logo.style.backgroundImage = "url('../img/logo-light.svg')";
+    logo.src = "../img/logo-light.svg";
+
+    heroLinkedIn.src = "../img/light-linkedin.svg";
+    heroGitHub.src = "../img/light-github.svg";
+    heroDribble.src = "../img/light-dribbble.svg";
+
+    connectLinkedIn.src = "../img/light-linkedin.svg";
+    connectGitHub.src = "../img/light-github.svg";
+    connectDribble.src = "../img/light-dribbble.svg";
+
     technologies.src ="img/light-technologies.svg";
     projects.style.backgroundImage = "url('../img/code-background.svg')";
     footer.classList.remove("dark");
